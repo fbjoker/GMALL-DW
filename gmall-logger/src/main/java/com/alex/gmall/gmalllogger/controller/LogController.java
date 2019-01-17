e9ea889d392697f55cfaec02d9c4e1a202e59596
fbjoker <39256912@qq.com>
2019-01-17 09:06:53 +0800

    0117

---
+++ gmall-logger/src/main/java/com/alex/gmall/gmalllogger/controller/LogController.java
@@ -0,0 +1,35 @@
+package com.alex.gmall.gmalllogger.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class LogController {
+
+    private static final  Logger logger = LoggerFactory.getLogger(LogController.class);
+
+    @PostMapping("/log")
+    public  String getLog(@RequestParam("log") String log){
+
+
+        JSONObject jsonObject = JSON.parseObject(log);
+
+        jsonObject.put("ts",System.currentTimeMillis());
+
+        String jsonstr = jsonObject.toJSONString();
+
+       // System.out.println(jsonstr);
+
+        logger.info(jsonstr);
+
+
+        return  null;
+
+
+    }
+}
