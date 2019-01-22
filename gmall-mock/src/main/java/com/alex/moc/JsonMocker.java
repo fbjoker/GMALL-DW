16069b7af443617edf8fc2e1f8207b6714925b65
fbjoker <39256912@qq.com>
2019-01-22 08:17:54 +0800

    0122

--- gmall-mock/src/main/java/com/alex/moc/JsonMocker.java
+++ gmall-mock/src/main/java/com/alex/moc/JsonMocker.java
@@ -120,8 +120,8 @@ public class JsonMocker {
      `area` string COMMENT '城市' */
 
 
-        String mid= "mid_"+ RandomNum.getRandInt(1,500);
-        String uid=""+ RandomNum.getRandInt(1,500);
+        String mid= "mid_"+ RandomNum.getRandInt(200,850);
+        String uid=""+ RandomNum.getRandInt(200,700);
         String os=osOptionGroup.getRandomOpt().getValue();
         String appid=this.appId;
         String area=areaOptionGroup.getRandomOpt().getValue();
