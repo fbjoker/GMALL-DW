d5628186c177fe34b8563ffe8dd460fc775b6fee
fbjoker <39256912@qq.com>
2019-01-24 09:59:42 +0800

    0124

--- gmall-logger/src/main/java/com/alex/gmall/gmalllogger/controller/LogController.java
+++ gmall-logger/src/main/java/com/alex/gmall/gmalllogger/controller/LogController.java
@@ -5,7 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
